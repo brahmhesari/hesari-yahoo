ac916adfc65116c86730bf33273b95a6f986bb3d
BRAHM <BRAHM@DESKTOP-P2FKPG8>
2018-10-17 11:55:02 -0400

    move yahoo progect to hub

---
+++ yahooproject/src/test/java/login/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author BRAHM
+ *
+ */
+package login;
---
+++ yahooproject/src/test/java/login/test1.java
@@ -0,0 +1,13 @@
+package login;
+
+public class test1 {
+
+	public static void main(String[] args) {
+
+System.out.println("this is test");
+			  
+			  
+		 
+	}
+
+}
